510e5c607e933ae27c282f41326f2d067fdcf49c
bumerboy86 <bumerboy86@gmail.com>
2024-06-26 20:00:43 +0500

    added vite conf base url

--- vite.config.ts
+++ vite.config.ts
@@ -3,6 +3,7 @@ import react from "@vitejs/plugin-react-swc";
 
 // https://vitejs.dev/config/
 export default defineConfig({
+  base: "/holod",
   plugins: [react()],
   css: {
     preprocessorOptions: {
